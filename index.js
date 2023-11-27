6d3d5f8961c94c765e317b8f9eafa745544e0271
stevensa1 <steven.apps.paid@gmail.com>
2023-11-27 19:44:27 +0700

    Retry to connect RabbitMQ if failed with maximum 5 attempt

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const bp = require("body-parser");
+const bp = require('body-parser');
 app.use(bp.json());
 
-const amqp = require("amqplib");
+const amqp = require('amqplib');
 const amqpServer = process.env.AMQP_URL;
-var channel, connection;
+let channel, connection;
 
 connectToQueue();
 
 async function connectToQueue() {
-    connection = await amqp.connect(amqpServer);
-    channel = await connection.createChannel();
     try {
-        const queue = "order";
+        connection = await amqp.connect(amqpServer);
+        channel = await connection.createChannel();
+        const queue = 'order';
         await channel.assertQueue(queue);
-        console.log("Connected to the queue!")
+        console.log('Connected to the queue!');
     } catch (ex) {
-        console.error(ex);
+        console.error(`Error while connecting to the queue: ${ex.message}`);
+        retryConnection(); // Retry the connection
     }
 }
 
-app.post("/order", (req, res) => {
+function retryConnection() {
+    console.log('Retrying connection in 5 seconds...');
+    setTimeout(connectToQueue, 5000); // Retry after 5 seconds
+}
+
+app.post('/order', (req, res) => {
     const { order } = req.body;
     createOrder(order);
     res.send(order);
 });
 
-const createOrder = async order => {
-    const queue = "order";
-    await channel.sendToQueue(queue, Buffer.from(JSON.stringify(order)));
-    console.log("Order succesfully created!")
-    process.once('SIGINT', async () => { 
-        console.log('got sigint, closing connection');
-        await channel.close();
-        await connection.close(); 
-        process.exit(0);
-    });
+const createOrder = async (order) => {
+    const queue = 'order';
+    try {
+        await channel.sendToQueue(queue, Buffer.from(JSON.stringify(order)));
+        console.log('Order successfully created!');
+    } catch (ex) {
+        console.error(`Error while sending order to the queue: ${ex.message}`);
+    }
 };
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running at ${process.env.PORT}`);
 });
 
+process.once('SIGINT', async () => {
+    console.log('Got SIGINT, closing connection');
+    await channel.close();
+    await connection.close();
+    process.exit(0);
+});
