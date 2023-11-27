1889c0f21d8d6fadf88e52a67a7010f1d86e6d84
stevensa1 <steven.apps.paid@gmail.com>
2023-11-27 19:45:20 +0700

    Retry to connect RabbitMQ if failed with maximum 5 attempt

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const app = express();
 
-const bp = require("body-parser");
+const bp = require('body-parser');
 
-const amqp = require("amqplib");
+const amqp = require('amqplib');
 const amqpServer = process.env.AMQP_URL;
-var channel, connection;
+let channel, connection;
 
-connectToQueue();
+const maxRetries = 5; // Maximum number of connection retry attempts
+let retryCount = 0;
 
 async function connectToQueue() {
     try {
         connection = await amqp.connect(amqpServer);
         channel = await connection.createChannel();
-        await channel.assertQueue("order");
-        channel.consume("order", data => {
+        await channel.assertQueue('order');
+        channel.consume('order', (data) => {
             console.log(`Order received: ${Buffer.from(data.content)}`);
-            console.log("** Will be shipped soon! **\n")
+            console.log('** Will be shipped soon! **\n');
             channel.ack(data);
         });
+        console.log('Connected to the queue successfully!');
+        retryCount = 0; // Reset retry count on successful connection
     } catch (ex) {
-        console.error(ex);
+        console.log(`Error while connecting to the queue: ${ex.message}`);
+        if (retryCount < maxRetries) {
+            retryCount++;
+            console.log(
+                `Retrying in 5 seconds (Retry ${retryCount}/${maxRetries})...`
+            );
+            setTimeout(connectToQueue, 5000); // Retry after 5 seconds
+        } else {
+            console.log('Maximum retry attempts reached. Exiting application.');
+            process.exit(1); // Exit the application after maximum retries
+        }
     }
 }
+
+connectToQueue(); // Initial connection attempt
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running at ${process.env.PORT}`);
